8e964700f79643107cc1e1dc3b4daf92846b124a
Lorenzo Patto <lorenzoqdpatto@gmail.com>
2023-07-31 21:25:42 -0300

    fix: remove useless lines

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,6 +1,4 @@
-import { Header } from '../../components/Header/index.tsx';
 import { RestaurantOption } from '../../components/RestaurantOption';
-import { TabBar } from '../../components/TabBar/index.tsx';
 import { Container } from "./styles.ts";
 
 export const Home = () => {
